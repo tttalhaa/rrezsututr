eecb420e381d6cb7fb3f1a24ad6736398ad89b80
tttalhaa <78103636+tttalhaa@users.noreply.github.com>
2021-08-21 15:25:05 +0200

    Update ana.py

--- ana.py
+++ ana.py
@@ -1,14 +1,15 @@
-Müşteri . on_message (
-    filtreler . özel
-    &  filtreler . komut ( "kullanım" )
+@Client.on_message(
+    filters.private
+    & filters.command("kullanim")
     )
-zaman uyumsuz  def  kl ( istemci , mesaj ):
-         mesaj bekliyoruz . answer_text ( f"""<b>
-Merhba🖐🏻, { mesaj . from_user . söz } şimdi sana bu bot nasıl kullanılır onu anlatıcam...
+async def kl(client, message):
+        await message.reply_text(f"""<b>
+Merhba🖐🏻, {message.from_user.mention} 
+Merhba🖐🏻, {message.from_user.mention} şimdi sana bu bot nasıl kullanılır onu anlatıcam...
 \n /atag - Adminleri etiketler Ör\:Mesajınızı yazın ve gönderin NOT:(/atag@LuciTagBot böyle değil.) 
 \n /all - Kullanıcıları etiketler Ör\:Mesajınızı yazın ve gönderin NOT:(/all@LuciTagBot böyle değil.) 
 \n /cancel - Etiketleme işlemini durdurur Ör\:Mesajınızı yazın ve gönderin NOT:(/cancel@LuciTagBot böyle değil.) 
 \n /kullanim - bot anlatım videosunu atar. 
 </b>""" )
-        zaman . uyku ( 1 )
-         müşteri bekle . send_video ( chat_id = mesaj . sohbet . id , video = "https://telegra.ph/file/cb18ca71b8a030b4c6652.mp4
+        time.sleep(1)
+        await client.send_video(chat_id=message.chat.id, video= "https://telegra.ph/file/cb18ca71b8a030b4c6652.mp4", caption="**@LuciTagBot'u nasıl kullanmanız gerektiği hakkında bilgi videosu...**")
