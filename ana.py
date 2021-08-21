f760cad5e7b332e18b23e7eacdd4801e5fb4d350
tttalhaa <78103636+tttalhaa@users.noreply.github.com>
2021-08-21 15:48:23 +0200

    Update ana.py

--- ana.py
+++ ana.py
@@ -8,7 +8,7 @@ Merhba🖐🏻, {message.from_user.mention} şimdi sana bu bot nasıl kullanıl�
 \n /atag - Adminleri etiketler Ör\:Mesajınızı yazın ve gönderin NOT:(/atag@LuciTagBot böyle değil.) 
 \n /all - Kullanıcıları etiketler Ör\:Mesajınızı yazın ve gönderin NOT:(/all@LuciTagBot böyle değil.) 
 \n /cancel - Etiketleme işlemini durdurur Ör\:Mesajınızı yazın ve gönderin NOT:(/cancel@LuciTagBot böyle değil.) 
-\n /kullanim - bot anlatım videosunu atar. 
+\n /kullanim - Botu nasıl kullanmanız gerektiği hakkında bilgi videosunu atar.
 </b>""" )
         time.sleep(1)
         await client.send_video(chat_id=message.chat.id, video= "https://telegra.ph/file/cb18ca71b8a030b4c6652.mp4", caption="**@LuciTagBot'u nasıl kullanmanız gerektiği hakkında bilgi videosu...**")
